620b75ebf2c1c175690fdad365f32b36ff2f72b3
Anas <anas.tajee2002@gmail.com>
2025-06-20 19:25:22 +0100

    feat: creating ProductFormPage & ProductListPage.

---
+++ src/pages/ProductFormPage.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ProductForm from '../components/ProductForm';
+
+const ProductFormPage: React.FC = () => <ProductForm />;
+
+export default ProductFormPage;
---
+++ src/pages/ProductListPage.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ProductList from '../components/ProductList';
+
+const ProductListPage: React.FC = () => <ProductList />;
+
+export default ProductListPage;
